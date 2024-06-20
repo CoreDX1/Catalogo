88fd4483366730ea5574e7a6c7f4285a4a4af96d
CoreDX1 <chrismaquis@gmail.com>
2024-06-20 10:24:23 -0300

    Mejorando la estructura

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,9 +1,26 @@
-using Catalogo.Models;
+using Catalogo.Models.Entities;
+using Catalogo.Models.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Catalogo.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ProductController : Controller { }
+public class ProductController : Controller
+{
+    private readonly IProductServices _productServices;
+
+    public ProductController(IProductServices productServices)
+    {
+        _productServices = productServices;
+    }
+
+    [HttpGet]
+    [Route("GetProducts")]
+    [ProducesResponseType(typeof(IEnumerable<Producto>), StatusCodes.Status200OK)]
+    public async Task<IActionResult> GetProducts()
+    {
+        var products = await _productServices.GetProducts();
+        return Ok(products);
+    }
+}
