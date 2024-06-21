c09343362d70e21d4cf424682fa920eae03a50c0
CoreDX1 <chrismaquis@gmail.com>
2024-06-21 16:21:03 -0300

    ⬇️ Angular
    Instalado Angular

--- obj/Debug/net8.0/Catalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Catalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0224b05d1f2c5cf551594a37a3857571399fe5ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8edb55bf953b30b63e5235b1b076ac996040d3d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
