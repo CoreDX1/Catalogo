d77f3b398c6374af8f086d550383270c36658696
CoreDX1 <chrismaquis@gmail.com>
2024-06-25 10:58:00 -0300

    Descargando imagenes y agregando a la base de datos

--- Queries/AlmacenDB.sql
+++ Queries/AlmacenDB.sql
@@ -52,6 +52,14 @@ values
     ('zapatillas_running_supernova_rise_rojo.jpg', 'zapatillas de running supernova rise'),
     ('zapatillas_running_blanco_standard.jpg', 'zapatillas de running 4dfwd x strung 4d');
 
+insert into Imagenes
+    (Imagen, NameImagen)
+values
+    ('zapatillas_running_switch_run_negro_standard.jpg','zapatillas running switch run'),
+    ('zapatillas_pureboost_23_Blanco_01_standard.jpg','zapatillas pureboots 23'),
+    ('zapatillas_duramo_azul_01_standard.jpg', 'zapatillas duramo sl');
+
+
 
 insert into Productos
     (Nombre, Descripcion, Precio, CategoriaID, ImagenID, Stars)
@@ -60,28 +68,20 @@ values
     ('zapatillas de running 4dfwd x strung 4d', 'Para pies delgados recomendamos comprar el talle inferior',378999.00, 1, 2, 4);
 
 
+insert into Productos
+    (Nombre, Descripcion, Precio, CategoriaID, ImagenID, Stars)
+values
+    ('zapatillas running switch run', 'ZAPATILLAS DE RUNNING LIVIANAS CON EXTERIOR DE MALLA Y AMARRE CON CORDONES.
+Fusionando estilo con detalles que evitan las distracciones, la última colección de prendas básicas de running adidas te mantendrá luciendo tan bien como te sientes mientras corres. Para que puedas concentrarte en lo que más importa: divertirte con cada paso.
+
+Amarrate las zapatillas de running Switch Run y disfrutá de una pisada con ritmo. Estas zapatillas están diseñadas para ofrecer un ajuste ligero y cuentan con la suela Adiwear y un refuerzo 3D en el talón para mayor estabilidad. El exterior de malla y el acolchado te garantizan máxima comodidad para tus pasos. Su exterior contiene al menos un 50% de materiales', 169999.00, 1, 1, 3),
+    ('zapatillas de running 4dfwd x strung 4d', 'Para pies delgados recomendamos comprar el talle inferior',378999.00, 1, 2, 4);
+
+
+select * from Imagenes;
 
     
 select C.Nombre, P.Nombre, P.Descripcion, I.Imagen,P.Precio from Productos as P
 join Imagenes as I on P.ImagenID = I.ImagenID
 join Categorias as C on P.CategoriaID = C.CategoriaID 
 where C.CategoriaID = 1;
-
-GO
-
-CREATE PROCEDURE GetProductosPorCategoria
-    @CategoriaID INT
-AS
-BEGIN
-    SET NOCOUNT ON;
-
-    SELECT C.Nombre AS Categoria,
-           P.Nombre AS Producto,
-           P.Descripcion,
-           I.Imagen,
-           P.Precio
-    FROM Productos P
-    JOIN Imagenes I ON P.ImagenID = I.ImagenID
-    JOIN Categorias C ON P.CategoriaID = C.CategoriaID
-    WHERE C.CategoriaID = @CategoriaID;
-END
