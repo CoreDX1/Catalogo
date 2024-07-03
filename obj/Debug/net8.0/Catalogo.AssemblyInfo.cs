86c5a5c25bf0cad4ededc5d4f11853a4a8efb5e9
CoreDX1 <chrismaquis@gmail.com>
2024-07-03 08:06:49 -0300

    REST

--- obj/Debug/net8.0/Catalogo.AssemblyInfo.cs
+++ obj/Debug/net8.0/Catalogo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalogo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d19ebd12138d320facd3903f880dd20b51bbe90f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b278c7546d318545caf042672105c78ca60ecab0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalogo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalogo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
